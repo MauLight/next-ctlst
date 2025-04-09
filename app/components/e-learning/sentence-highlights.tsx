73593d727911553e0c93b56d6915dad78e44e1d0
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-04-09 09:24:56 -0400

    feat: fixed highlights, again
    change to client side, orchestrate racing conditions

--- app/components/e-learning/sentence-highlights.tsx
+++ app/components/e-learning/sentence-highlights.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface SentenceHighlightsProps {
     text: string
@@ -11,12 +14,17 @@ interface SentenceHighlightsProps {
 export default function SentenceHighlights({ text, highlights, bgImages, length }: SentenceHighlightsProps) {
 
     const textArray = text.split(' ')
-    const style = `absolute w-full h-full top-0 left-0 grid grid-cols-${length} -z-10`
+    const [style, setStyle] = useState<string | null>(null)
 
-    console.log(length)
+    useEffect(() => {
+        if (length) {
+            setStyle(`absolute w-full h-full top-0 left-0 grid grid-cols-${length} -z-10`)
+        }
+    }, [])
 
     return (
-        <div className='relative min-h-screen flex items-center'>
+        <div className='relative h-screen flex items-center overflow-hidden'>
+
             <div className='flex flex-wrap items-center z-10 text-[1.618rem] gap-x-2 w-full max-w-[980px]'>
 
                 {
@@ -28,20 +36,26 @@ export default function SentenceHighlights({ text, highlights, bgImages, length 
                 }
 
             </div>
-            <div className={style}>
-                {
-                    bgImages.map((image, i) => (
-                        <div key={`key-${image.id}-${i}-${length}`} className='w-full'>
-                            <Image className='object-cover w-full h-full opacity-18' src={image.img as string} width={12800} height={720} alt='bg' />
-                        </div>
-                    ))
-                }
-                {
-                    Array.from({ length: 3 }).map((_, i) => (
-                        <div key={i} className='absolute top-0 left-0 w-full h-full bg-radial from-transparent from-10% to-[#0a0a0a]'></div>
-                    ))
-                }
-            </div>
+
+            {
+                style !== null && (
+                    <div className={style}>
+                        {
+                            bgImages.map((image, i) => (
+
+                                <Image key={`key-${image.id}-${i}-${length}`} className='col-span-1 object-cover w-full h-full opacity-18' src={image.img as string} width={1280 / length} height={720} alt='bg' />
+
+                            ))
+                        }
+                        {
+                            Array.from({ length: 3 }).map((_, i) => (
+                                <div key={i} className='absolute top-0 left-0 w-full h-full bg-radial from-transparent from-10% to-[#0a0a0a]'></div>
+                            ))
+                        }
+                    </div>
+                )
+            }
+
         </div>
     )
 }
