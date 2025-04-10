1e7fbc19ea0b4ed114143417873a218b819990b3
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-04-10 09:14:58 -0400

    feat: added dropdown container

--- app/components/e-learning/dropdown.tsx
+++ app/components/e-learning/dropdown.tsx
@@ -3,8 +3,18 @@
 import { useState, type ReactNode } from 'react'
 import { AnimatePresence, motion } from 'motion/react'
 
-import ComponentWrapper from '../common/component-wrapper'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
+
+interface LessonProps {
+    id: string
+    text: string
+    completion: number
+}
+
+interface DropdownProps {
+    title: string,
+    lessons: Array<LessonProps>
+}
 
 const containerVariants = {
     initial: { height: 'auto' },
@@ -24,79 +34,60 @@ const childVariants = {
     exit: { height: 0, opacity: 0 }
 }
 
-const lessons = [
-    {
-        id: 'a1',
-        text: 'What is a screenwriter?',
-        completion: 90
-    },
-    {
-        id: 'b2',
-        text: 'About the movie industry.',
-        completion: 90
-    },
-    {
-        id: 'c3',
-        text: 'Is this course for you?',
-        completion: 90
-    },
-]
-
-export default function Dropdown(): ReactNode {
+export default function Dropdown({ title, lessons }: DropdownProps): ReactNode {
 
     const [hover, setHover] = useState<boolean>(false)
 
     return (
-        <ComponentWrapper>
 
-            <motion.div
-                onHoverStart={() => { setHover(true) }}
-                onHoverEnd={() => { setHover(false) }}
-                className='group w-full flex flex-col justify-center overflow-hidden gap-y-5'>
 
-                <div className="flex w-full justify-between items-center">
-                    <h1>Course Introduction</h1>
-                    {
-                        hover ? (
-                            <ChevronUpIcon className='w-5 h-5' />
-                        )
-                            :
-                            (
-                                <ChevronDownIcon className='w-5 h-5' />
-                            )
-                    }
-                </div>
+        <motion.div
+            onHoverStart={() => { setHover(true) }}
+            onHoverEnd={() => { setHover(false) }}
+            className='group w-full flex flex-col justify-center overflow-hidden gap-y-5'>
 
+            <div className="flex w-full justify-between items-center">
+                <h1>{title}</h1>
                 {
-                    hover && (
-                        <AnimatePresence mode='wait'>
-                            <motion.div
-                                key={'container'}
-                                variants={containerVariants}
-                                initial='initial'
-                                animate='hover'
-                                exit='exit'
-                                className='w-full flex flex-col justify-center overflow-hidden gap-y-3'>
-                                {
-                                    lessons.map((lesson, i) => (
-                                        <motion.div
-                                            key={lesson.id}
-                                            variants={childVariants}
-                                            className='flex justify-start items-center gap-x-2 text-text-secondary hover:text-sky-400 cursor-pointer'>
-                                            <div className='w-7 h-7 flex justify-center items-center shrink-0 rounded-full border border-border text-[0.8rem]'>
-                                                {i + 1}
-                                            </div>
-                                            <p>{lesson.text}</p>
-                                        </motion.div>
-                                    ))
-                                }
-                            </motion.div>
-                        </AnimatePresence>
+                    hover ? (
+                        <ChevronUpIcon className='w-5 h-5' />
                     )
+                        :
+                        (
+                            <ChevronDownIcon className='w-5 h-5' />
+                        )
                 }
+            </div>
 
-            </motion.div>
+            {
+                hover && (
+                    <AnimatePresence mode='wait'>
+                        <motion.div
+                            key={'container'}
+                            variants={containerVariants}
+                            initial='initial'
+                            animate='hover'
+                            exit='exit'
+                            className='w-full flex flex-col justify-center overflow-hidden gap-y-3'>
+                            {
+                                lessons.map((lesson, i) => (
+                                    <motion.div
+                                        key={lesson.id}
+                                        variants={childVariants}
+                                        className='flex justify-start items-center gap-x-2 text-text-secondary hover:text-sky-400 cursor-pointer'>
+                                        <div className='w-7 h-7 flex justify-center items-center shrink-0 rounded-full border border-border text-[0.8rem]'>
+                                            {i + 1}
+                                        </div>
+                                        <p>{lesson.text}</p>
+                                    </motion.div>
+                                ))
+                            }
+                        </motion.div>
+                    </AnimatePresence>
+                )
+            }
 
-        </ComponentWrapper>
+        </motion.div>
+
     )
 }
--- app/courses/the-quest/page.tsx
+++ app/courses/the-quest/page.tsx
@@ -1,13 +1,83 @@
 
+import ComponentWrapper from '@/app/components/common/component-wrapper'
 import Dropdown from '@/app/components/e-learning/dropdown'
 import { type ReactNode } from 'react'
 
+
+const lessons1 = [
+    {
+        id: 'a1',
+        text: 'What is a screenwriter?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'b2',
+        text: 'About the movie industry.',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'c3',
+        text: 'Is this course for you?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+]
+const lessons2 = [
+    {
+        id: 'a1',
+        text: 'What is a screenwriter?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'b2',
+        text: 'About the movie industry.',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'c3',
+        text: 'Is this course for you?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+]
+const lessons3 = [
+    {
+        id: 'a1',
+        text: 'What is a screenwriter?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'b2',
+        text: 'About the movie industry.',
+        completion: 90,
+        url: '/lesson-1'
+    },
+    {
+        id: 'c3',
+        text: 'Is this course for you?',
+        completion: 90,
+        url: '/lesson-1'
+    },
+]
+
 export default function Page(): ReactNode {
+    const upperPadding = 400 - (60 * 3)
     return (
-        <div className='w-full flex flex-col gap-y-20 justify-center items-center'>
+        <div style={{ paddingTop: `${upperPadding}px` }} className='w-full min-h-[calc(100vh-60px)] flex flex-col justify-start items-center'>
 
 
-            <Dropdown />
+            <ComponentWrapper>
+                <div className='w-full flex flex-col gap-y-8'>
+                    <Dropdown title='Course Introduction' lessons={lessons1} />
+                    <Dropdown title='The Basics of Story' lessons={lessons2} />
+                    <Dropdown title='Course Introduction' lessons={lessons3} />
+                </div>
+            </ComponentWrapper>
 
         </div>
     )
